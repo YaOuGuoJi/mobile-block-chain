bda8572b33e16b567e95e8edc104e5e878a5fc3d
Morse <1185585454@qq.com>
2018-11-13 18:12:36 +0800

    addMy

---
+++ src/page/PowerBase.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="power-base">
+    <common-header :title="title" :showback="true"></common-header>
+    <span>暂未开放</span>
+  </div>
+</template>
+
+<script>
+  import commonHeader from '../components/common-header'
+
+  export default {
+    data() {
+      return {
+        title: '算力基地'
+      }
+    },
+    components: {
+      commonHeader
+    }
+  }
+</script>
+
+<style scoped>
+  span {
+    position: fixed;
+    left: 40%;
+    top: 40%;
+    font-size: 20px
+  }
+</style>
