4b358581704db5fe29fd96e52262ec5eb87622d5
Hut <42726605+lijingwei007@users.noreply.github.com>
2018-11-14 17:08:44 +0800

    save

--- src/page/UserInfo.vue
+++ src/page/UserInfo.vue
@@ -1,8 +1,31 @@
 <template>
   <div class="user-info">
-    <common-header :title="title" :showback="true"></common-header>
+    <div>
+      <common-header :title="title" :showback="true"></common-header>
+    </div>
+    <div style="width: 100%;height:55px;"></div>
+    <div style="width: 100%;height:200px;">
+      <div>
+        <img ref="../assets/imgs/default.jpg" style="width: 50%;height:140px;border-radius:50%"/>
+        <br>
+        <span style="font-size: 20px">{{userInfo.userName}}</span><br/>
+        <span style="font-size: 20px">{{userInfo.vip}}</span>
+      </div>
+    </div>
+    <span>个人简介：</span>
+    <hr style="background-color: #7e8c8d;width: 90%;height: 1px">
+    <div style="text-align:left;margin-left: 10%">
+      <span>生日：{{buildBirth(userInfo.birthday)}}</span><br/>
+      <span>性别：{{userInfo.sex === 1 ? "man":"girl"}}</span><br/>
+      <span>电话：{{userInfo.phone}}</span><br/>
+      <span>邮箱：{{userInfo.email}}</span><br/>
+      <span>工作：{{userInfo.job}}</span><br/>
+      <span>人生格言:</span><br/>
+    </div>
     <div class="userInfo">
-      {{ userInfo }}
+      <!--{{ userInfo }}-->
+
+
     </div>
   </div>
 </template>
@@ -18,7 +41,7 @@
         userInfo: null
       }
     },
-    created () {
+    created() {
       service('get', 'user/detail', {}).then(response => {
         if (response.code !== 200 || !response.data) {
           alert(response.message);
@@ -28,6 +51,17 @@
     },
     components: {
       commonHeader
+    },
+    methods: {
+      buildBirth(str) {
+        let date = new Date(str),
+          // 月份从0开始，需要+1
+          month = date.getMonth() + 1,
+          day = date.getDate()
+        return (month < 10 ? '0' + month : month) + '月' +
+          (day < 10 ? '0' + day : day) + '号'
+
+      }
     }
   }
 </script>
@@ -36,5 +70,9 @@
   .userInfo {
     position: relative;
     top: 20%;
+  }
+
+  span {
+    font-size: 20px;
   }
 </style>
